cfabba5bdecd07898094993c66166200ce8774bd
Steven Holmes <steven.w.holmes@gmail.com>
2016-11-03 08:21:01 -0500

    cleaned up code + made adjustments per instructor comments

--- quiz.js
+++ quiz.js
@@ -1,37 +1,13 @@
+///// INITIAL VARIABLES
 
-var treeLimbs = {}; // empty object to hold user input
+var treeLimbs = {}; // Object to hold user input on 'click' or 'keypress' event
 var growButton = document.getElementById("grow"); // Variable representing button in DOM
 var space = " ";
 
-growButton.addEventListener("click", tree);
+///// EVENT LISTENERS
 
-function tree(treeLimbs) {
-	console.log("Click event"); // console log click event
-	treeLimbs.leaves = document.getElementById("field").value; // user input -> object
-	treeLimbs.height = document.getElementById("height").value; // user input -> object
-	var char = []; // creating empty array for tree building blocks
-	var spaceAmt = treeLimbs.height;
-	console.log(treeLimbs);
+growButton.addEventListener("click", growTree);
 
-	for (var i = 0; i < treeLimbs.height; i++) {
-		var charStack = (i * 2) + 1; //number of characters printed per iteration
-		spaceAmt -= 1; // with each iteration, one less space is printed
-		console.log(space.repeat(spaceAmt) + treeLimbs.leaves.repeat(charStack));
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 field.addEventListener("keyup", function(keyPress){ // Event listener on text field for 'enter' keypress
 	var chars = document.getElementById("field").value
 	keyPress.preventDefault();
@@ -45,24 +21,40 @@ field.addEventListener("keyup", function(keyPress){ // Event listener on text fi
 height.addEventListener("keyup", function(keyPress){ // Event listener on text field for 'enter' keypress
 	var integer = document.getElementById("height");
 	keyPress.preventDefault();
-	if(keyPress.keyCode < 48 && keyPress.keyCode !== 8 || keyPress.keyCode > 57 && keyPress.keyCode != 8) {
+	if(keyPress.keyCode < 48 && keyPress.keyCode !== 8 && keyPress.keyCode !== 13 || keyPress.keyCode > 57 && keyPress.keyCode !== 8 && keyPress.keyCode !== 13) {
 		alert("Enter only integers, plz."); // Prevents user from entering anything other than an integer
 	} else if (keyPress.keyCode == 13) {
 		document.getElementById("grow").click();
 	};
 });
 
+///// FUNCTIONS
+
 function tree(treeLimbs) {
-	var treeLimbs = { // object to hold user input
-		leaves: document.getElementById("field").value, // user input
-		height: document.getElementById("height").value // user input
-	};
+	console.log("Click event"); // console log click event
+	treeLimbs.leaves = document.getElementById("field").value; // user input -> object
+	treeLimbs.height = document.getElementById("height").value; // user input -> object
+	var char = []; // creating empty array for tree building blocks
 	var spaceAmt = treeLimbs.height;
+	console.log(treeLimbs);
 
 	for (var i = 0; i < treeLimbs.height; i++) {
-		var charStack = (i * 2) + 1; // Number of characters printed per iteration
-		spaceAmt -= 1; // With each iteration, one less space is printed
+		var charStack = (i * 2) + 1; // number of characters printed per iteration
+		spaceAmt -= 1; // with each iteration, one less space is printed
 		console.log(space.repeat(spaceAmt) + treeLimbs.leaves.repeat(charStack));
 	}
 }
->>>>>>> exercise
+
+function growTree() {
+	treeLimbs.leaves = document.getElementById("field").value; // user input -> object
+	treeLimbs.height = document.getElementById("height").value; // user input -> object
+	tree(treeLimbs);
+}
+
+///// TREE-BUILDING LOOP
+
+for (var i = 0; i < treeLimbs.height; i++) {
+	var charStack = (i * 2) + 1; // Number of characters printed per iteration
+	spaceAmt -= 1; // With each iteration, one less space is printed
+	console.log(space.repeat(spaceAmt) + treeLimbs.leaves.repeat(charStack));
+}
